468b210a37f2de7a043f8739c61e6c2dd437771c
Ahd <dafuq.ahd@gmail.com>
2020-05-06 20:02:54 +0300

    Updates

---
+++ precache-manifest.7a81adb0d1a548ca64e05543ab4cf0dd.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba67c8f9b220c0d23322a9062a30088",
+    "url": "/fashionfe/index.html"
+  },
+  {
+    "revision": "1a4465e92a3e8d994559",
+    "url": "/fashionfe/static/css/main.54f6c324.chunk.css"
+  },
+  {
+    "revision": "0ca540e43b01bf261761",
+    "url": "/fashionfe/static/js/2.f1eecb45.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fashionfe/static/js/2.f1eecb45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a4465e92a3e8d994559",
+    "url": "/fashionfe/static/js/main.ec3f4467.chunk.js"
+  },
+  {
+    "revision": "0860b416e934e796028c",
+    "url": "/fashionfe/static/js/runtime-main.4a67db30.js"
+  },
+  {
+    "revision": "dfaf153a14ac72a4e7b7e167ed7972aa",
+    "url": "/fashionfe/static/media/abaya5.dfaf153a.jpg"
+  },
+  {
+    "revision": "31565960ee3eedea975eecc0a8cfbd69",
+    "url": "/fashionfe/static/media/abaya6.31565960.jpg"
+  },
+  {
+    "revision": "fb51ff397eee3527a6266fa5ad2d6af5",
+    "url": "/fashionfe/static/media/abaya7.fb51ff39.jpg"
+  },
+  {
+    "revision": "89b58cdb36a815533326f31b8696c1f2",
+    "url": "/fashionfe/static/media/background.89b58cdb.png"
+  },
+  {
+    "revision": "17391b8d9a33bdf481d8031a39092565",
+    "url": "/fashionfe/static/media/formbg.17391b8d.png"
+  },
+  {
+    "revision": "bfbc82a0b56c149e0458a40a003ebe63",
+    "url": "/fashionfe/static/media/newsletterbg.bfbc82a0.png"
+  },
+  {
+    "revision": "1d4c05bb2e34413450684c74bbb1546c",
+    "url": "/fashionfe/static/media/sbgbackground2.1d4c05bb.png"
+  },
+  {
+    "revision": "8785f31a192f6f2f194260edd371d762",
+    "url": "/fashionfe/static/media/svgbg3.8785f31a.png"
+  },
+  {
+    "revision": "daec9fda010428056eecbc5d59561d35",
+    "url": "/fashionfe/static/media/svgcut3.daec9fda.png"
+  },
+  {
+    "revision": "390e168209fc5ef55668f88b6ee12dba",
+    "url": "/fashionfe/static/media/svgcutdress.390e1682.png"
+  },
+  {
+    "revision": "b66fcc7957ab14748b419ffa9da84c21",
+    "url": "/fashionfe/static/media/svgcutmiddle.b66fcc79.png"
+  },
+  {
+    "revision": "2afee0e4cbd8327bf6eaa55685a77cce",
+    "url": "/fashionfe/static/media/svgpurple.2afee0e4.png"
+  }
+]);
