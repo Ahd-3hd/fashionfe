e067984597f7ab8b870cc912433119ce73174f8a
Ahd <dafuq.ahd@gmail.com>
2020-05-06 20:04:25 +0300

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Background from "./components/Background";
+import Navbar from "./components/Navbar";
+import Promotion from "./components/Promotion";
+import Newsletter from "./components/Newsletter";
+import Footer from "./components/Footer";
+import Store from "./components/Store";
+import Product from "./components/Product";
+import NewPost from "./components/NewPost";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <div className="App">
+            <Background />
+            <Header />
+            <Promotion />
+            <Newsletter />
+          </div>
+        </Route>
+        <Route exact path="/store" component={Store} />
+        <Route path="/store/1" component={Product} />
+        <Route path="/new" component={NewPost} />
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
